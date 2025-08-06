129fb7d63c96af26e4e77d24bfb44df14df3b120
KUCHIKI <linceasap@outlook.com>
2025-08-06 09:08:26 +0300

    Main login page for all student acounts

---
+++ app/loginpages/students/page.tsx
@@ -0,0 +1,31 @@
+
+'use client';
+
+import LoginForm from '@/app/components/loginForm';
+import {useRouter} from 'next/navigation';
+export default function StudentLoginPage() {
+    const router = useRouter()
+
+const handleLogin = async (credentials: any) => {
+
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/login`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    credentials: "include",
+    body: JSON.stringify({
+     ...credentials,
+     role:"student"
+    }),
+  })
+
+  const data = await res.json()
+  if (res.ok) {
+  
+    router.push(`/dashboards/${data.role}`) // e.g., /dashboard/student
+  } else {
+    console.log(data.error)
+  }
+}
+
+  return <LoginForm title="Student Login" onLogin={handleLogin} />;
+}
