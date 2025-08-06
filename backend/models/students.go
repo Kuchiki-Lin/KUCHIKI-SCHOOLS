ca934502acf2db49de7a03ed5d74e02fe43aa37b
KUCHIKI <linceasap@outlook.com>
2025-08-06 09:51:40 +0300

    Update backend/models/students.go

---
+++ backend/models/students.go
@@ -0,0 +1,19 @@
+
+package models
+import(
+	"time"
+)
+
+type Student struct {
+	ID                uint   `json:"id" gorm:"primaryKey"`
+	Username          string `json:"username"`
+	Password          string `json:"password"` // Store hashed version
+	Role              string `json:"role"`
+	FullName          string `json:"fullname"`
+	RegistrationNumber string `json:"registrationNumber"`
+	Department string `json:"department"` 
+	Age               int    `json:"age"`
+	Year              string `json:"year"`
+	
+	CreatedAt time.Time `json:"created_at"`
+}
