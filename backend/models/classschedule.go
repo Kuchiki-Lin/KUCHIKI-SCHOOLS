1c541318d1c4337a86003ca055582b9ffb9c7281
KUCHIKI <linceasap@outlook.com>
2025-08-06 09:51:38 +0300

    Update backend/models/classschedule.go

---
+++ backend/models/classschedule.go
@@ -0,0 +1,17 @@
+package models
+
+import "time"
+
+type ClassSchedule struct {
+	ID        uint      `json:"id" gorm:"primaryKey"`
+	TeacherID int      `json:"teacher_id"`
+	CourseID  uint      `json:"course_id"`
+	DayOfWeek string    `json:"day_of_week"` // "Monday"
+	StartTime string    `json:"start_time"`  // "12:00"
+	EndTime   string    `json:"end_time"`
+	CourseCode string `  json:"course_code"`
+    CourseName string     `json:"course_name"`
+	Venue     string    `json:"venue"`
+	Semester  string    `json:"semester"`
+	CreatedAt time.Time
+}
